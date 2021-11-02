4c5567fe2a736d29d5095039c1c20b12b2d7793f
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-11-02 11:40:22 -0700

    updated schemas to match act 18

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+
+   
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, { username }) => {
+      const foundUser = await User.findOne(username)
+      if (!foundUser) {
+        throw new AuthenticationError('Cannot find a user with this id!');
+      }
+      return foundUser;
+    }
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return{ token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Can't find this user" )
+      }
+      const correctPw = await user.isCorrectPassword(password);
+  
+      if (!correctPw) {
+        throw new AuthenticationError('Wrong password!')
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { authors, description, title, bookId, image, userId }) => {
+        try {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: userId },
+            { $addToSet: { savedBooks: {authors, description, title, bookId, image} } },
+            { new: true, runValidators: true }
+          );
+          return updatedUser;
+        } catch (err) {
+          console.log(err);
+          return;
+        }
+    },
+    removeBook: async (parent, { user, params }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "Couldn't find user with this id!" });
+      }
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me(username: String!): User
+  }
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, userId: String): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
