71724c7b6d108f17255eeaf9a02ffc523f8e97ce
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-11-02 11:55:05 -0700

    added typedefs into mutations

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login() {
-      login() {
+  mutation login($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
           token
           user {
 
@@ -12,8 +12,8 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-   mutation createUser() {
-      createUser() {
+   mutation createUser($username: String!, $email: String!, $password: String!) {
+      createUser(username: $username, email: $email, password: $password) {
           token
           user {
               
@@ -23,15 +23,15 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-mutation saveBook() {
-    saveBook() {
+mutation saveBook($authors: [String], $description: String!, $title: String!, $bookId: String!, $image: String, $userId: String) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, userId:$userId) {
     }
 }
 `;
 
 export const REMOVE_BOOK = gql`
-mutation removeBook() {
-    removeBook() {
+mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
     }
 }
 `;
